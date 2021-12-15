ebe3819759eb2e076c8d1813d960cfb1407c3a5a
George Lamb <georgeashleylamb@gmail.com>
2021-12-15 11:40:06 +1100

    Add util scripts

---
+++ util/create_release.bat
@@ -0,0 +1,6 @@
+make config=release
+md release
+copy res.pck release
+copy bin\openmv release
+copy bin\core.dll release
+copy logic.dll release
---
+++ util/create_release.sh
@@ -0,0 +1,7 @@
+make config=release
+./bin/packer
+mkdir -p release
+cp res.pck release
+cp bin/openmv release
+cp bin/libcore.so release
+cp liblogic.so release
