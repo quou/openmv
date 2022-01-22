7821e0dd22f97f09dab718d51109fb411e95f99f
George Lamb <georgeashleylamb@gmail.com>
2022-01-22 13:09:47 +1100

    Add API before script functions

--- core/src/lsp.h
+++ core/src/lsp.h
@@ -22,14 +22,14 @@ struct lsp_val {
 #define lsp_make_num(n_)         ((struct lsp_val) { .type = lsp_val_num,  .as.num = n_ })
 #define lsp_make_bool(b_)        ((struct lsp_val) { .type = lsp_val_bool, .as.boolean = b_ })
 
-struct lsp_state* new_lsp_state(void* error, void* info);
-void free_lsp_state(struct lsp_state* state);
+API struct lsp_state* new_lsp_state(void* error, void* info);
+API void free_lsp_state(struct lsp_state* state);
 
-void lsp_set_simple_errors(struct lsp_state* state, bool simple);
+API void lsp_set_simple_errors(struct lsp_state* state, bool simple);
 
-void           lsp_push(struct lsp_state* ctx, struct lsp_val val);
-struct lsp_val lsp_pop(struct lsp_state* ctx);
-struct lsp_val lsp_peek(struct lsp_state* ctx);
+API void           lsp_push(struct lsp_state* ctx, struct lsp_val val);
+API struct lsp_val lsp_pop(struct lsp_state* ctx);
+API struct lsp_val lsp_peek(struct lsp_state* ctx);
 
-struct lsp_val lsp_do_string(struct lsp_state* ctx, const char* str);
-struct lsp_val lsp_do_file(struct lsp_state* ctx, const char* file_path);
+API struct lsp_val lsp_do_string(struct lsp_state* ctx, const char* str);
+API struct lsp_val lsp_do_file(struct lsp_state* ctx, const char* file_path);
